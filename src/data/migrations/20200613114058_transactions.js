e63f5878ed216e11b2cfadfc2012a5dbc4d535cc
iamcristos <nmereginivincent@gmail.com>
2020-06-13 12:37:29 +0100

    completed contibutions model

--- src/data/migrations/20200613114058_transactions.js
+++ src/data/migrations/20200613114058_transactions.js
@@ -3,6 +3,10 @@ exports.up = function (knex) {
     tbl.increments();
     tbl.integer('amount')
       .notNullable();
+    tbl.timestamp('createdAt', {
+      precision: 6,
+    })
+      .defaultTo(knex.fn.now(6));
     tbl.integer('contributionId')
       .unique()
       .unsigned()
---
+++ src/model/contribution.js
@@ -0,0 +1,35 @@
+import knex from 'knex';
+import CommonQuery from './common';
+import dbConfig from '../data/dbConfig';
+
+class Contribution extends CommonQuery {
+  constructor(table) {
+    super(table);
+    this.db = dbConfig(table);
+  }
+
+  addContribution({ newBalance, oldBalance, driverId }) {
+    return knex.transaction((trx) => {
+      this.db.transacting(trx)
+        .where('driverId', driverId)
+        .update({ currentBalance: newBalance, previousBalance: oldBalance })
+        .returning('id')
+        .then((res) => {
+          console.log(res, 'first response');
+          const body = { amount: newBalance - oldBalance, contributionId: res };
+          return dbConfig('transactions').insert(body).returning('*');
+        })
+        .then((complete) => trx.commit().then(() => complete))
+        .catch(trx.rollback)
+        .then((resp) => {
+          console.log('DBG99 Insert Transaction complete. Returns:', resp);
+          return resp; // if you want the value returned.
+        })
+        .catch((err) => {
+          console.error('DBG89 Insert Transaction ERROR:', err);
+        });
+    });
+  }
+}
+
+export default Contribution;
