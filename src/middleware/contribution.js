452bd2de89cb0eabcda8e1a8d4f95d648c0b3abf
iamcristos <nmereginivincent@gmail.com>
2020-06-13 12:49:40 +0100

    completed contribution middleware

---
+++ src/middleware/contribution.js
@@ -0,0 +1,30 @@
+import Models from '../model';
+
+class Contribution {
+  static async create(req, res, next) {
+    try {
+      const { id } = req.params;
+      const contribution = await Models().contribution.findOne('driverId', id);
+      if (!contribution) return next();
+      return res.status(400).json({ message: 'driver already have an account' });
+    } catch (error) {
+      return next(error.message);
+    }
+  }
+
+  static async find(req, res, next) {
+    try {
+      const { id } = req.params;
+      const contribution = await Models().contribution.findOne('driverId', id);
+      if (!contribution) {
+        return res.status(400).json({ message: 'driver do not have an account' });
+      }
+      req.contribution = contribution;
+      return next();
+    } catch (error) {
+      return next(error.message);
+    }
+  }
+}
+
+export default Contribution;
